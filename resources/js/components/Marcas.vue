84699d382cc3894631fc62461551287b9a9ce336
Erick Germani <erickgermani13@gmail.com>
2023-01-07 03:07:42 -0300

    Início do desenvolvimento do front-end

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Busca de marcas</div>
+
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mb-3">
+                                <input-container-component></input-container-component>
+
+                                <label for="inputId" class="form-label">
+                                    ID
+                                </label>
+                                <input
+                                    type="number"
+                                    class="form-control"
+                                    id="inputId"
+                                    aria-describedby="idHelp"
+                                    placeholder="ID"
+                                />
+                                <div id="idHelp" class="form-text">
+                                    Opcional. Informe o ID do registro.
+                                </div>
+                            </div>
+                            <div class="col mb-3">
+                                <label for="inputNome" class="form-label"
+                                    >Nome
+                                </label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="inputNome"
+                                    aria-describedby="nomeHelp"
+                                    placeholder="Nome da marca"
+                                />
+                                <div id="nomeHelp" class="form-text">
+                                    Opcional. Informe o nome da marca.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <button
+                            type="submit"
+                            class="btn btn-primary btn-sm float-end"
+                        >
+                            Pesquisar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script></script>
